75ff823259d9287f7ef38d30a95502f5956ab0a1
Yeshwanth115 <53565614+Yeshwanth115@users.noreply.github.com>
2019-08-09 12:16:12 +0530

    Create current date and time.py

---
+++ current date and time.py
@@ -0,0 +1,7 @@
+import datetime
+now=datetime.datetime.now()
+print('Current date andtime:')
+print(now.strftime("%y-%m-%d %H:%M:%S"))import datetime
+now=datetime.datetime.now()
+print('Current date andtime:')
+print(now.strftime("%y-%m-%d %H:%M:%S"))
